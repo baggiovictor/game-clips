ff1ac8333257efcb607dc1ccca3d095061a51334
Victor Baggio <baggiobaggiovictor@gmail.com>
2022-11-03 21:11:56 -0300

    feat: add firebase in project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC8Xdz1bxlxQ51qzSqXLiR3jCMuiBEqNfI",
+    authDomain: "game-clips-1c93a.firebaseapp.com",
+    projectId: "game-clips-1c93a",
+    storageBucket: "game-clips-1c93a.appspot.com",
+    appId: "1:1058207985344:web:f8ce7ee99bec1799e24725"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC8Xdz1bxlxQ51qzSqXLiR3jCMuiBEqNfI",
+    authDomain: "game-clips-1c93a.firebaseapp.com",
+    projectId: "game-clips-1c93a",
+    storageBucket: "game-clips-1c93a.appspot.com",
+    appId: "1:1058207985344:web:f8ce7ee99bec1799e24725"
+  }
 };
 
 /*
