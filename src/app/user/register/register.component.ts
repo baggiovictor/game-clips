6468ff8be49dc0ac66a4c454f1e14f245fff14a6
Victor Baggio <baggiobaggiovictor@gmail.com>
2022-11-03 21:31:00 -0300

    feat: configurado a criacao de conta com o firebase

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,37 +1,33 @@
-import {Component} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms'
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  name = new FormControl('', [
-    Validators.required,
-    Validators.minLength(3)
-  ]);
-  email = new FormControl('', [
-    Validators.required,
-    Validators.email
-  ]);
+  constructor(private auth: AngularFireAuth) {}
+
+  inSubmission = false;
+
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
   age = new FormControl('', [
     Validators.required,
     Validators.min(18),
-    Validators.max(120)
+    Validators.max(120),
   ]);
   password = new FormControl('', [
     Validators.required,
-    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ]);
-  confirm_password = new FormControl('', [
-    Validators.required,
-
-  ]);
+  confirm_password = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('', [
     Validators.required,
     Validators.minLength(14),
-    Validators.maxLength(14)
+    Validators.maxLength(14),
   ]);
 
   showAlert = false;
@@ -44,14 +40,32 @@ export class RegisterComponent {
     age: this.age,
     password: this.password,
     confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber
+    phoneNumber: this.phoneNumber,
   });
 
-
-  register() {
+  async register() {
     this.showAlert = true;
-    this.alertMsg = 'Please wait! Yout account is being created.'
-    this.alertColor = 'blue'
-    console.log(this.registerForm);
+    this.alertMsg = 'Please wait! Yout account is being created.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+    const { email, password } = this.registerForm.value;
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+        email as string,
+        password as string
+      );
+      console.log(userCred);
+    } catch (e) {
+      console.error(e);
+
+      this.alertMsg = 'An unexpected error occured. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMsg='Success! Your account has been created.'
+    this.alertColor = 'green'
   }
 }
